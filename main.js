52274dcc95298c1e76ec6c895df1f8088230199c
tanishkadaramoni8 <75026809+tanishkadaramoni8@users.noreply.github.com>
2020-12-11 19:18:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+menu_list_array = ["Chicken Tandoori Pizza",
+                   "Veg Supreme Pizza",
+                   "Panner Tikka Pizza",
+                   "Deluxe Veggi Pizza",
+                   "Veg Extravaganza Pizza"];
+
+
+function getmenu() 
+{
+    var htmldata;
+    htmldata = "<ol class='menulist'>"
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++)
+    {
+        htmldata = htmldata+ '<li>' + menu_list_array[i] + '</li>'
+    }
+    htmldata = htmldata+"<ol>"
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+
+
+function add_item() 
+{
+    var htmldata;
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>"
+    for(var i=0;i<menu_list_array.length;i++)
+    {
+        htmldata=htmldata+ '<div class="cards">' +"<img src='pizzaImg.png'id='ttt'/>" + menu_list_array[i] + '</div>'
+    }
+     htmldata=htmldata+"</section>"
+     document.getElementById("display_addmenu").innerHTML = htmldata;
+}
